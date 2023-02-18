f0e76e82d0223c21adc6bfe62e27283a4491351b
guruemms <123012403+guruemms@users.noreply.github.com>
2023-02-18 15:34:33 -0700

    Almost

--- code_4.py
+++ code_4.py
@@ -1,30 +1,35 @@
 #import csv
 import pandas as pd
 
-'''
+
 def get_big_mac_price_by_year(year,country_code):
-    query1 = f"(iso_a3 == {country_code} and str(date[0:4]) == {year}"
-    price_by_year = df['dollar_price'].query(query1)
-    print(round(price_by_year),2)
+    query1 = f"(iso_a3 == {country_code} and date >= '{year}-01-01' and date <= '{year}-12-31'"
+    price_by_year = df.query(query1)
+    avg_p_year = price_by_year.mean()
+    print(round(avg_p_year),2)
 
 def get_big_mac_price_by_country(country_code):
-    query1 = f"(iso_a3 == {country_code}"
-    price_by_country = df['dollar_price'].query(query1)
-    print(round((price_by_country.mean()),2))
-'''
+    country_code = country_code.upper()
+    query1 = f"(iso_a3 == {country_code})"
+    pby = df.query(query1)
+    avg_p_year = pby.mean()
+    print(round(avg_p_year,2))
+
 def get_the_cheapest_big_mac_price_by_year(year):
-    query1 = f"date[0:4] == '{year}'"
+    query1 = f"date >= '{year}-01-01' and date <= '{year}-12-31'"
     pby_df = df.query(query1) #price by year dataframe
-    print(price_by_year)
-    idx_min_price = price_by_year['dollar_price'].idxmin()
-    country = df['country'].loc[idx_min_price]
-    price = df['dollar_price'].loc[idx_min_price]
-    print(country,price)
-'''
+    idx_min_price = pby_df['dollar_price'].idxmin()
+    min_price = df.loc[idx_min_price]
+    print(f"{min_price['name']}({min_price['iso_a3']}): ${round(min_price['dollar_price'],2)}")
+
 def get_the_most_expensive_big_mac_price_by_year(year):
-    #query1 = f"str(date[0:4]) == {year}"
-'''
+    query1 = f"date >= '{year}-01-01' and date <= '{year}-12-31'"
+    pby_df = df.query(query1) #price by year dataframe
+    idx_max_price = pby_df['dollar_price'].idxmax()
+    max_price = df.loc[idx_max_price]
+    print(f"{max_price['name']}({max_price['iso_a3']}): ${round(max_price['dollar_price'],2)}")
+
 if __name__ == "__main__":
     big_mac_file = './big-mac-full-index.csv'
     df = pd.read_csv(big_mac_file)
-    get_the_cheapest_big_mac_price_by_year(2008)
+    get_big_mac_price_by_year(2008,'JPN')
