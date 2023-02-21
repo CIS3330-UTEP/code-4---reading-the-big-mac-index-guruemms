8338ec57ebc604021e5a57eef48d685d9cfea8c2
guruemms <123012403+guruemms@users.noreply.github.com>
2023-02-21 11:10:08 -0700

    Push

--- code_4.py
+++ code_4.py
@@ -3,25 +3,18 @@ import pandas as pd
 
 
 def get_big_mac_price_by_year(year,country_code):
-    big_mac_file = './big-mac-full-index.csv'
-    df = pd.read_csv(big_mac_file)
-    query1 = f"(iso_a3 == {country_code} and date >= '{year}-01-01' and date <= '{year}-12-31'"
-    price_by_year = df.query(query1)
-    avg_p_year = price_by_year.mean()
-    print(round(avg_p_year),2)
+    query1 = f"(iso_a3 == {country_code.upper()} and date >= '{year}-01-01' and date <= '{year}-12-31'"
+    pby = df.query(query1)
+    pby['dollar_price'].mean()
+    print(round(pby['dollar_price'].mean()),2)
 
 def get_big_mac_price_by_country(country_code):
-    big_mac_file = './big-mac-full-index.csv'
-    df = pd.read_csv(big_mac_file)
     country_code = country_code.upper()
     query1 = f"(iso_a3 == {country_code})"
     pby = df.query(query1)
-    avg_p_year = pby.mean()
-    print(round(avg_p_year,2))
+    print(round(pby['dollar_price'].mean(),2))
 
 def get_the_cheapest_big_mac_price_by_year(year):
-    big_mac_file = './big-mac-full-index.csv'
-    df = pd.read_csv(big_mac_file)
     query1 = f"date >= '{year}-01-01' and date <= '{year}-12-31'"
     pby_df = df.query(query1) #price by year dataframe
     idx_min_price = pby_df['dollar_price'].idxmin()
@@ -29,7 +22,6 @@ def get_the_cheapest_big_mac_price_by_year(year):
     print(f"{min_price['name']}({min_price['iso_a3']}): ${round(min_price['dollar_price'],2)}")
 
 def get_the_most_expensive_big_mac_price_by_year(year):
-    big_mac_file = './big-mac-full-index.csv'
     df = pd.read_csv(big_mac_file)
     query1 = f"date >= '{year}-01-01' and date <= '{year}-12-31'"
     pby_df = df.query(query1) #price by year dataframe
@@ -38,4 +30,6 @@ def get_the_most_expensive_big_mac_price_by_year(year):
     print(f"{max_price['name']}({max_price['iso_a3']}): ${round(max_price['dollar_price'],2)}")
 
 if __name__ == "__main__":
-    pass
+    big_mac_file = './big-mac-full-index.csv'
+    df = pd.read_csv(big_mac_file)
+    get_big_mac_price_by_year(2008,'arg')
