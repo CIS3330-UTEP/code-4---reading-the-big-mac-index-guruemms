34477ec7508e46362046837fc103c886d029b71d
guruemms <123012403+guruemms@users.noreply.github.com>
2023-02-21 11:50:58 -0700

    Final Push?

--- code_4.py
+++ code_4.py
@@ -14,14 +14,16 @@ def get_big_mac_price_by_country(country_code):
     country_code = country_code.upper()
     query1 = f"(iso_a3 == '{country_code}')"
     pby = df.query(query1)
-    print(round(pby['dollar_price'].mean(),2))
+    bmpby = round(pby['dollar_price'].mean(),2)
+    return bmpby
 
 def get_the_cheapest_big_mac_price_by_year(year):
     query1 = f"date >= '{year}-01-01' and date <= '{year}-12-31'"
     pby_df = df.query(query1) #price by year dataframe
     idx_min_price = pby_df['dollar_price'].idxmin()
     min_price = df.loc[idx_min_price]
-    print(f"{min_price['name']}({min_price['iso_a3']}): ${round(min_price['dollar_price'],2)}")
+    cbmpy = f"{min_price['name']}({min_price['iso_a3']}): ${round(min_price['dollar_price'],2)}")
+    return cbmpy
 
 def get_the_most_expensive_big_mac_price_by_year(year):
     df = pd.read_csv(big_mac_file)
@@ -29,11 +31,7 @@ def get_the_most_expensive_big_mac_price_by_year(year):
     pby_df = df.query(query1) #price by year dataframe
     idx_max_price = pby_df['dollar_price'].idxmax()
     max_price = df.loc[idx_max_price]
-    print(f"{max_price['name']}({max_price['iso_a3']}): ${round(max_price['dollar_price'],2)}")
+    ebmpy = f"{max_price['name']}({max_price['iso_a3']}): ${round(max_price['dollar_price'],2)}"
+    return ebmpy
 
 if __name__ == "__main__":
-    print(get_big_mac_price_by_year(2008,'jpn'))
-'''    
-
-    get_big_mac_price_by_year(2008,'arg')
-'''
